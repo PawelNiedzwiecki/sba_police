cde66ec871cd38af69f38191d6004dd7fdec5786
Duvel <duvelmapsa@gmail.com>
2019-10-27 20:51:33 +0100

    Added implementation for Adding Project to Database, Added "Delete project" from list and new component MessageBox

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,24 +3,116 @@ import {
   Link,
   useRouteMatch, 
 } from 'react-router-dom';
+import DatePicker from 'react-date-picker';
+import MessageBox from './MessageBox.js';
 
 const Add = () => {
-  let match = useRouteMatch();
+  // let match = useRouteMatch();
 
-  console.log(match);
+  // console.log(match);
+  const 
+    [date, setDate] = useState(),
+    [projectName, setProjectName] = useState(),
+    [projectLink, setProjectLink] = useState(),
+    [sbaLink, setSbaLink] = useState(),
+    [isCreating, setIsCreating] = useState(),
+    [respCode, setRespCode] = useState(),
+    [messageType, setMessageType] = useState(),
+    [message, setMessage] = useState();
+
+    function createProject(){
+      
+      if(projectName){
+      setIsCreating(1)
+      if (date) {
+        const day = date.getDate() < 10 ? '0'+date.getDate() : date.getDate(),
+              month = date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1,
+              year = date.getFullYear();
+
+        const newDate = `${day}.${month}.${year}`;
+
+        fetch(`http://justsimply.pl/sba/api/wdrozenie/create.php?nazwa=${projectName}&linkProjektu=${projectLink}&linkSBA=${sbaLink}&dataZakonczenia='${newDate}'`)
+        .then((response) => {
+          setRespCode(response.status);
+          if(response.status == 400) {
+            setMessageType("danger");
+            setMessage("Nie udało się dodać wdrożenia");
+          }else{
+            setMessageType("success");
+            setMessage("Dodano wdrożenie");
+          }
+          response.json();
+        })
+          .then((resp) => {
+            setIsCreating(0);
+          })
+
+      }else{
+        fetch(`http://justsimply.pl/sba/api/wdrozenie/create.php?nazwa=${projectName}&linkProjektu=${projectLink}&linkSBA=${sbaLink}`)
+          .then((response) => {
+            setRespCode(response.status);
+            if(response.status == 400) {
+              setMessageType("danger");
+              setMessage("Nie udało się dodać wdrożenia");
+            }else{
+              setMessageType("success");
+              setMessage("Dodano wdrożenie");
+            }
+            response.json();
+          })
+            .then((resp) => {
+              setIsCreating(0);
+            })
+      }
+    }else{
+      setMessage("Musisz podać nazwę wdrożenia");
+      setMessageType("danger");
+    }
+
+    }
+
+    function resetFields(){
+      setDate('');
+      setProjectName('');
+      setProjectLink('');
+      setSbaLink('');
+      setRespCode('');
+      setIsCreating('');
+      setMessage('');
+    }
+
 
   return (
-  <div className="test">
-    <h2>Dodaj wdrozenie</h2>
-    <div className="wrapping">
-      <div className="input-group flex-nowrap">
+  <div className="AddWrapper">
+    <MessageBox message={message} messageType={messageType}/>
+    <h2 className="mb-5">Dodaj wdrozenie</h2>
+    <div className="wrapping row">
+      <div className="input-group flex-nowrap mb-4 col-md-12">
         <div className="input-group-prepend">
           <span className="input-group-text" id="addon-wrapping">Nazwa</span>
         </div>
-        <input type="text" className="form-control" placeholder="Nazwa wdrozenia" aria-label="ProjectName" aria-describedby="addon-wrapping" />
+        <input type="text" className="form-control" placeholder="Nazwa wdrozenia" aria-label="ProjectName" aria-describedby="addon-wrapping" value={projectName} onChange={(e)=> setProjectName(e.target.value)}/>
       </div>
-      <div className="input-group flex-nowrap">
-        
+      <div className="input-group flex-nowrap mb-4 col-md-6">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="addon-wrapping">https://</span>
+        </div>
+        <input type="text" className="form-control" placeholder="Link wdrożenia" aria-label="ProjectLink" aria-describedby="addon-wrapping" value={projectLink} onChange={(e) => setProjectLink(e.target.value)} />
+      </div>
+      <div className="input-group flex-nowrap mb-4 col-md-6">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="addon-wrapping">https://</span>
+        </div>
+        <input type="text" className="form-control" placeholder="Link SBA" aria-label="SBALink" aria-describedby="addon-wrapping" value={sbaLink} onChange={(e) => setSbaLink(e.target.value)}/>
+      </div>
+      <div className="input-group flex-nowrap mb-4 col-md-6">
+        <label className="d-inline-flex mr-4">Data zakończenia:</label>
+        <DatePicker onChange={(data)=>{setDate(data)}} value={date}/>
+      </div>
+      
+      <div className="buttons-wrapper col-md-12">
+        <button type="submit" className="btn btn-primary mr-2" onClick={createProject}>Dodaj</button>
+        <button type="button" className="btn btn-outline-danger" onClick={resetFields}>Reset</button>
       </div>
     </div>
   </div>
--- src/components/ListaWdrozen.js
+++ src/components/ListaWdrozen.js
@@ -2,34 +2,68 @@ import React, { useState, useEffect } from 'react';
 import {
   Link,
 } from 'react-router-dom';
+import MessageBox from './MessageBox.js';
 
 const ListaWdrozen = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]),
+        [message, setMessage] = useState(),
+        [messageType, setMessageType] = useState();
 
   useEffect(() => {
     fetch('http://justsimply.pl/sba/api/wdrozenie/read.php').then((res) => res.json()).then((result) => setData(result.wdrozenia));
   }, []);
-  
-  return (
-    <div>
-      <h2>Lista wdrożeń</h2>
-      <ul className="list-group">
-        {data.map((item) => (
-          <li key={item.id} className="list-group-item">
-            <Link to={{
-              pathname: `/wdrozenie/${item.id}`,
-              state: { idWdrozenia: item.id },
-            }}
-            >
-              {item.nazwa}
 
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-  );
+  function deleteProject(id){
+    fetch(`http://justsimply.pl/sba/api/wdrozenie/delete.php?id=${id}`)
+      .then((response) =>{
+        console.log(response.status);
+        fetch('http://justsimply.pl/sba/api/wdrozenie/read.php').then((res) => res.json()).then((result) => setData(result.wdrozenia));
+        if(response.status == 401){
+          setMessage("Nie udało się usunać wdrożenia");
+          setMessageType("danger");
+        }else{
+          setMessage("Usunięto wdrożenie");
+          setMessageType("success");
+        }
+      })
   }
+  
+  if(data){
+    return (
+      <div>
+        <MessageBox message={message} messageType={messageType}/>
+        <h2 className="mb-4">Lista wdrożeń</h2>
+        <ul className="list-group">
+          {data.map((item) => (
+            <li key={item.id} data-id={item.id} className="list-group-item">
+              <Link to={{
+                pathname: `/wdrozenie/${item.id}`,
+                state: { idWdrozenia: item.id },
+              }}
+              >
+                {item.nazwa}
+
+              </Link>
+              <button type="button" class="close" aria-label="Close" onClick={(e) => deleteProject(e.target.dataset.id)}>
+                <span aria-hidden="true" data-id={item.id}>&times;</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    );
+  }else{
+    return(
+      <div>
+        <MessageBox message={message} messageType={messageType} />
+        <h2 className="mb-4">Lista wdrożeń</h2>
+        <ul className="list-group">
+          <li className="list-group-item">Brak wdrożeń</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
   export default ListaWdrozen;
